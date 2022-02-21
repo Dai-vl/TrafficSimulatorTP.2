b063f089916870bdd061d87173eaa65ce05f2791
Dai-vl <dvidal03@ucm.es>
2022-02-21 17:48:49 +0100

    Move Strategies

--- src/simulator/model/MoveAllStrategy.java
+++ src/simulator/model/MoveAllStrategy.java
@@ -1,13 +1,12 @@
 package simulator.model;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MoveAllStrategy implements DequeuingStrategy {
 
 	public List<Vehicle> dequeue(List<Vehicle> q) {
-		List<Vehicle> list = new ArrayList<>(q); // TODO preguntar a iker
-		return list;
+		return Collections.unmodifiableList(q);
 	}
 
 }
--- src/simulator/model/MoveFirstStrategy.java
+++ src/simulator/model/MoveFirstStrategy.java
@@ -3,13 +3,14 @@ package simulator.model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MoveFirstStrategy implements DequeuingStrategy{
+public class MoveFirstStrategy implements DequeuingStrategy {
 
 	public List<Vehicle> dequeue(List<Vehicle> q) {
-		List<Vehicle> list = new ArrayList<>() ;
-		
-		list.add(q.get(0)); //TODO ?
-		
+		List<Vehicle> list = new ArrayList<>();
+
+		if (q.size() > 0)
+			list.add(q.get(0));
+
 		return list;
 	}
 
